e23deb1ed0d6aa1b483accbda76621fdeb47c737
Surabhi Srivastava <72458190+SurabhiSrivastav@users.noreply.github.com>
2023-03-21 20:52:11 +0530

    Create Designer.jsx

---
+++ client/src/components/Designer/Designer.jsx
@@ -0,0 +1,69 @@
+import "./design.scss";
+import { TextField, Button, Container, Stack,Box} from '@mui/material';
+
+
+const design = () => {
+  return (
+      <div class="container">
+          <h2>Upload Design</h2>
+      <Container fixed>
+        <Box sx={{ height: '78vh' }}> 
+      <form>
+          <TextField
+            type="text"
+            variant='outlined'  
+            label="Product Name"
+            required
+            fullWidth
+            sx={{ marginBottom: 4 }}
+          />
+          <TextField
+            type="text"
+            variant='outlined'
+            label="Description"
+            required
+            fullWidth
+            multiline={true}
+            sx={{ marginBottom: 4 }}
+          />
+        <TextField
+          type="text"
+          variant='outlined'
+          label="Price"
+          required
+          fullWidth
+          sx={{ marginBottom: 4 }}
+        />
+        <Stack spacing={2} direction="row" sx={{marginBottom: 4}}> 
+        <TextField
+          type="file"
+          variant='outlined'
+          label="Product Image"
+          required
+          fullWidth
+          sx={{ marginBottom: 2 }}
+          focused
+        />
+        <TextField
+          type="file"
+          variant='outlined'
+          label="Product Design"
+          required
+          fullWidth
+          sx={{ marginBottom: 2 }}
+          focused
+        />
+        </Stack>
+        
+        <Button variant="contained" color="success" type="submit">Upload</Button>
+        <Button variant="contained" color="error" type="cancel" sx={{marginLeft:2}}>cancel</Button>
+      </form>
+      </Box>
+      </Container>
+
+
+    </div>
+  );
+};
+
+export default design;
