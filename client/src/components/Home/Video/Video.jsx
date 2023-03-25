85a032214fd45437effaadaa7f70bd4151bb6939
Surabhi Srivastava <72458190+SurabhiSrivastav@users.noreply.github.com>
2023-03-25 20:35:11 +0530

    Update Video.jsx

--- client/src/components/Home/Video/Video.jsx
+++ client/src/components/Home/Video/Video.jsx
@@ -1,28 +1,22 @@
 import "./Video.scss";
+import ReactPlayer from 'react-player';
+import VideoPlayer from "react-video-js-player";
 import Vide from "../../../assets/outfitv.mp4";
+import Poster from "../../../assets/video_banner.webp";
 const Video = () => {
+    const videoSrc = Vide;
+    const poster = Poster
     return <div className="hero-vdo">
-         
+      
         <div className="content">
-            <video className="intro-vdo" width="320" height="240" controls
-                src={Vide} type="video/mp4">
-            </video>
-
             <div className="text-content">
-            <h4>HOW IT WORKS</h4>
-                <h3>See for yourself.</h3>
-                <p><b>Quick video introduction</b></p>
-            <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vitae
-            odales lectus, non ultrices nisi. In varius, leo non gravida.
-            </p>
+                <h5>HOW IT WORKS</h5>
+                <h2>See for yourself.</h2>
+                <h4>Quick video introduction</h4>
+                <div className="vdo-cta">Know More</div>
                 
-            <div className="ctas">
-                <div className="vdo-cta">Read More</div>
-                {/* <div className="vdo-cta v2">Shop Now</div> */}
             </div>
-            </div>
-           
+            <VideoPlayer src={videoSrc} poster={poster} width="520" height="272"/>
         </div>
     </div>
 };
